a542019a7b0b85f060167c6018be910ddb639eea
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-09-04 10:14:08 +0530

    submittion

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import UserList from "./UserList";
+import UserList from "./Userlist";
 import UserDetails from "./UserDetails";
 
 function App() {
